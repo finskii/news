faaf5a15983ef1bf9e01000278def387627ea228
Finskii <finskiidt@gmail.com>
2019-11-25 20:55:52 +0300

    add: BaseNVC

---
+++ news/Views/Base/BaseNVC.h
@@ -0,0 +1,17 @@
+//
+//  BaseNVC.h
+//  news
+//
+//  Created by Vasilii Dmitriev on 25.11.2019.
+//  Copyright © 2019 Vasilii Dmitriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaseNVC : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
